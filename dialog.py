da6d382424e6120b905dc9843bb6c5f0c80193c9
ulrichvonrekkenin <ulrihlebedev@gmail.com>
2014-10-20 14:03:30 +0400

    Some Fix.

--- dialog.py
+++ dialog.py
@@ -7,10 +7,8 @@ from tools import tools
 from tools.uidialog import Ui_TestDialog
 
 
-#-----------------------------------------------
-class TestDialog( QDialog ):
-    #-----------------------------------------------
-    def __init__( self, parent = None ):
+class TestDialog(QDialog):
+    def __init__(self, parent=None):
         QDialog.__init__(self)
 
         self.ui = Ui_TestDialog()
@@ -19,13 +17,14 @@ class TestDialog( QDialog ):
         self.TestCase = 0
         self.JSONFile = 'dialog.json'
 
-        self.sTestCase = ['Place the letters on correct order.',
-                          'Insert the correct letters.',
-                          '',
-                          'Match the words.',
-                          'Place the words in sentences on correct order.',
-                          'Place the sentences on correct order.',
-                         ]
+        self.sTestCase = [
+            'Place the letters on correct order.',
+            'Insert the correct letters.',
+            '',
+            'Match the words.',
+            'Place the words in sentences on correct order.',
+            'Place the sentences on correct order.'
+        ]
 
         self.ui.btnTestGenerate.clicked.connect(self.TestGenerate)
         self.connect(self.ui.cbTestCase, SIGNAL("activated(int)"), self.UpdateUiByTestCase)
@@ -33,8 +32,7 @@ class TestDialog( QDialog ):
         self.LoadDialogFromPickle()
 
 
-    #-----------------------------------------------
-    def TestGenerate( self ):
+    def TestGenerate(self):
 
         if len(self.ui.mMemoIn.toPlainText()):
             self.ui.mMemoOut.clear()
@@ -44,95 +42,96 @@ class TestDialog( QDialog ):
 
                 if self.TestCase in [0, 1, 2]:
 
-                        results  = []
-                        words = tuple(filter(len, self.ui.mMemoIn.toPlainText().split('\n')))
-                        
-                        if self.TestCase == 0:
-                            for word in words:
-                                results.append(tools.RandomOrderOfLetters(word).RandomLetters())
+                    results = []
+                    words = tuple(filter(len, self.ui.mMemoIn.toPlainText().split('\n')))
 
-                        elif self.TestCase == 1:
-                            for word in words:
-                                results.append(tools.ReplaceSomeLettersByUnderline(word).ReplaceLetters())
+                    if self.TestCase == 0:
+                        for word in words:
+                            results.append(tools.RandomOrderOfLetters(word).RandomLetters())
 
-                        elif self.TestCase == 2:
-                            for word in words:
-                                results.append(
-                                    tools.MissSpecialLetters(
-                                        word, self.ui.edMissedLetters.text()
-                                        ).MissLetters()
-                                    )
+                    elif self.TestCase == 1:
+                        for word in words:
+                            results.append(
+                                tools.ReplaceSomeLettersByUnderline(word).ReplaceLetters()
+                            )
 
-                        foo = '\n'.join('{}) {}'.format(*r) for r in enumerate(results, start=1))
+                    elif self.TestCase == 2:
+                        for word in words:
+                            results.append(
+                                tools.MissSpecialLetters(
+                                    word, self.ui.edMissedLetters.text()
+                                ).MissLetters()
+                            )
+
+                    foo = '\n'.join('{}) {}'.format(*r) for r in enumerate(results, start=1))
 
 
                 elif self.TestCase == 3:
-                    foo = tools.MatchCoupleOfWords(self.ui.mMemoIn.toPlainText()).Matching()
+                    foo = tools.MatchCoupleOfWords(
+                        self.ui.mMemoIn.toPlainText()
+                    ).Matching()
 
                 elif self.TestCase == 4:
                     foo = tools.RandomOrderOfWordsInSentences(
-                            self.ui.mMemoIn.toPlainText()
-                        ).RandomWords()
+                        self.ui.mMemoIn.toPlainText()
+                    ).RandomWords()
 
                 elif self.TestCase == 5:
                     foo = tools.RandomOrderOfSentenceInParagraph(
-                            self.ui.mMemoIn.toPlainText()
-                        ).RandomSentence()
+                        self.ui.mMemoIn.toPlainText()
+                    ).RandomSentence()
 
                 self.ui.mMemoOut.insertPlainText(
-                        'Variant #{0}\n{1}\nNAME:\nCLASS:\n{2}\n\n'.format(var+1, self.sTestCase[self.TestCase], foo)
-                    )
-
-    #-----------------------------------------------
-    def UpdateUiByTestCase( self ):
-       self.TestCase = self.ui.cbTestCase.currentIndex()
-
-       if self.TestCase in [0, 3, 4]:
-           self.ui.edMissedLetters.setEnabled(False)
-           self.ui.spUnderscorePart.setEnabled(False)
-       elif self.TestCase == 1:
-           self.ui.edMissedLetters.setEnabled(False)
-           self.ui.spUnderscorePart.setEnabled(True)
-       elif self.TestCase == 2:
-           self.ui.edMissedLetters.setEnabled(True)
-           self.ui.spUnderscorePart.setEnabled(False)
-
-
-    #-----------------------------------------------
-    def UpdateMissedLetters( self ):
-        self.sTestCase[2] = 'Insert correct letters: "{}"'.format(
-                str.join(
-                        ',',
-                        [str(x) for x in self.ui.edMissedLetters.text()]
+                    'Variant #{0}\n{1}\nNAME:\nCLASS:\n{2}\n\n'.format(
+                        var + 1, self.sTestCase[self.TestCase], foo
                     )
                 )
 
 
-    #-----------------------------------------------
-    def closeEvent( self, event ):
+    def UpdateUiByTestCase(self):
+        self.TestCase = self.ui.cbTestCase.currentIndex()
+
+        if self.TestCase in [0, 3, 4]:
+            self.ui.edMissedLetters.setEnabled(False)
+            self.ui.spUnderscorePart.setEnabled(False)
+        elif self.TestCase == 1:
+            self.ui.edMissedLetters.setEnabled(False)
+            self.ui.spUnderscorePart.setEnabled(True)
+        elif self.TestCase == 2:
+            self.ui.edMissedLetters.setEnabled(True)
+            self.ui.spUnderscorePart.setEnabled(False)
+
+
+    def UpdateMissedLetters(self):
+        self.sTestCase[2] = 'Insert correct letters: "{}"'.format(
+            str.join(
+                ',',
+                [str(x) for x in self.ui.edMissedLetters.text()]
+            )
+        )
+
+
+    def closeEvent(self, event):
         ''' Event::closeEvent = When app is closed save the state. '''
         import json
 
         with open(self.JSONFile, 'w') as output:
-            json.dump(
-                {
-                   'MemoIn': str(self.ui.mMemoIn.toPlainText()),
-                   'MemoOut': str(self.ui.mMemoOut.toPlainText()),
-                   'Variant': int(self.ui.spVariantNumber.text()),
-                   'Missed': str(self.ui.edMissedLetters.text()),
-                   'Underscore': str(self.ui.spUnderscorePart.text()),
-                   'TestCase': int(self.ui.cbTestCase.currentIndex())
-                },
+            json.dump({
+                'MemoIn': str(self.ui.mMemoIn.toPlainText()),
+                'MemoOut': str(self.ui.mMemoOut.toPlainText()),
+                'Variant': int(self.ui.spVariantNumber.text()),
+                'Missed': str(self.ui.edMissedLetters.text()),
+                'Underscore': str(self.ui.spUnderscorePart.text()),
+                'TestCase': int(self.ui.cbTestCase.currentIndex())},
                 output
             )
 
 
-    #-----------------------------------------------
-    def LoadDialogFromPickle( self ):
+    def LoadDialogFromPickle(self):
         import json
 
         if isfile(self.JSONFile) and getsize(self.JSONFile):
-            d = json.load( open(self.JSONFile, 'r') )
+            d = json.load(open(self.JSONFile, 'r'))
 
             self.ui.mMemoIn.clear()
             self.ui.mMemoIn.insertPlainText(d['MemoIn'])
@@ -153,4 +152,3 @@ if __name__ == "__main__":
     window.show()
 
     sys.exit(app.exec_())
-
--- tools/tools.py
+++ tools/tools.py
@@ -1,63 +1,74 @@
 import random
 
-def Shuffle( sequence ):
+
+def Shuffle(sequence):
     ''' Return a shuffled sequence. '''
     import copy
     seqin = copy.copy(sequence)
-    while (sequence == seqin): random.shuffle(sequence)
+    while (sequence == seqin):
+        random.shuffle(sequence)
     return sequence
 
 
-class RandomOrderOfLetters( object ):
+class RandomOrderOfLetters(object):
     ''' Make letters in a random order of a given word. '''
-    def __init__( self, word ):
+    def __init__(self, word):
         self.word = word
 
-    def RandomLetters( self ):
-        return str.join('', [str(x) for x in Shuffle(list(self.word))])
+    def RandomLetters(self):
+        return ''.join([str(x) for x in Shuffle(list(self.word))])
 
 
-class ReplaceSomeLettersByUnderline( object ):
+class ReplaceSomeLettersByUnderline(object):
     ''' Replace some letters by _ in a given word. '''
-    def __init__( self, word, percent = 50 ):
+    def __init__(self, word, percent=50):
         self.word = word
         self.percent = percent
         self.lenght = len(self.word)
 
-    def ReplaceLetters( self ):
-        self.rword = list(self.word)
+    def ReplaceLetters(self):
 
-        for i in random.sample( range(self.lenght), int(self.lenght * self.percent / 100.0) ):
-            self.rword[i] = "_"
-        return str.join(' ', [str(x) for x in self.rword])
+        ret = ''
+
+        for split_word in self.word.split():
+            rword = list(split_word)
+            for i in random.sample(
+                range(len(split_word)),
+                int(len(split_word) * self.percent / 100.)
+            ):
+                rword[i] = " __ "
+            ret += ''.join([str(x) for x in rword]) + 5 * ' '
+
+        return ret[:-5]
 
 
-class MissSpecialLetters( object ):
+class MissSpecialLetters(object):
     ''' Miss special letters in a given word. '''
-    def __init__( self, word, special = 'aeiouy' ):
+    def __init__(self, word, special='aeiouy'):
         self.word = word
         self.special = special
 
-    def MissLetters( self ):
+    def MissLetters(self):
         self.rword = list(self.word)
 
         for i, letter in enumerate(list(self.word)):
             if letter in self.special:
                 self.rword[i] = '_'
 
-        return str.join(' ', [str(x) for x in self.rword])
+        return ' '.join([str(x) for x in self.rword])
 
 
-class MatchCoupleOfWords( object ):
+
+class MatchCoupleOfWords(object):
     '''M...'''
-    def __init__( self, lines = 'foo : bar' ):
+    def __init__(self, lines='foo : bar'):
         self.col1, self.col2 = [], []
 
         for line in tuple(filter(len, lines.split('\n'))):
             self.col1.append(str(line.split(':')[0]))
             self.col2.append(str(line.split(':')[1]))
 
-    def Matching( self ):
+    def Matching(self):
         if len(self.col1) > 1 or len(self.col2) > 1:
             Shuffle(self.col1)
             Shuffle(self.col2)
@@ -65,47 +76,48 @@ class MatchCoupleOfWords( object ):
             self.result = ''
 
             for i in range(len(self.col1)):
-                self.result += '{0}) {1}\t\t{0}) {2}\n'.format(i+1, self.col1[i].strip(), self.col2[i].strip())
+                self.result += '{0}) {1}\t\t{0}) {2}\n'.format(
+                    i + 1, self.col1[i].strip(), self.col2[i].strip()
+                )
 
             return self.result
         else:
             return 'There is no way to shuffle. Please, add more pairs.'
 
-class RandomOrderOfWordsInSentences( object ):
+
+class RandomOrderOfWordsInSentences(object):
     ''' Randomize all words in a given sentences. '''
-    def __init__( self, sentences = 'This is the first simple sentence.      And another one.' ):
+    def __init__(self, sentences=''):
         self.sentences = tuple(filter(len, sentences.split('.')))
 
-    def RandomWords( self ):
+    def RandomWords(self):
 
         self.result = ''
 
-        for i, sentence in enumerate(self.sentences, start = 1):
+        for i, sentence in enumerate(self.sentences, start=1):
             self.result += '{0}) {1}.\n'.format(
-                    i, str.join(' ', Shuffle(
-                        list(
-                            filter(len, tuple(
-                                filter(len, str(sentence).split(' '))
-                                ))
-                            )
+                i, str.join(' ', Shuffle(
+                    list(
+                        filter(len, tuple(
+                            filter(len, str(sentence).split(' '))
                         ))
                     )
+                ))
+            )
 
         return self.result
 
 
 
-###----------------------------------------------
-class RandomOrderOfSentenceInParagraph( object ):
+class RandomOrderOfSentenceInParagraph(object):
     ''''''
-    def __init__( self, sentences = 'This is the first simple sentence.      And another one.' ):
+    def __init__(self, sentences=''):
         self.sentences = list(filter(len, str(sentences).split('.')))
 
-    def RandomSentence( self ):
+    def RandomSentence(self):
         self.result = ''
 
-        for i, sentence in enumerate(Shuffle(self.sentences), start = 1):
+        for i, sentence in enumerate(Shuffle(self.sentences), start=1):
             self.result += '{}) {}.\n'.format(i, sentence)
 
         return self.result
-
